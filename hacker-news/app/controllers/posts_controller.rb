a86b286a57323a7fb65f74c7064ce286a8d8553a
Quyen Tran <email": null>
2017-05-29 14:32:17 -0700

    Add nesting action between posts and users

--- hacker-news/app/controllers/posts_controller.rb
+++ hacker-news/app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+    @user = User.find(session[:user_id])
   end
 
   def new
--- hacker-news/db/schema.rb
+++ hacker-news/db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529201427) do
+ActiveRecord::Schema.define(version: 20170529205943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -23,4 +34,5 @@ ActiveRecord::Schema.define(version: 20170529201427) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "posts", "users"
 end
