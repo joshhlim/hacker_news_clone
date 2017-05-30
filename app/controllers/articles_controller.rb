c342df93e59f1520258f1dcc298e22ff2cdb7094
Paul Vander <paulsvander@gmail.com>
2017-05-29 22:10:46 -0700

    Edit article now working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,15 @@ class ArticlesController < ApplicationController
 
   def new
     @category = Category.find(params[:category_id])
-    respond_with(@article)
   end
 
   def create
     @category = Category.find(params[:category_id])
     @article = @category.articles.create(article_params)
     @article.make_key
-    @article.save
+    if  @article.save
+      flash[:notice] = "USE THIS URL TO EDIT/DELETE YOUR POST: #{request.original_url}/#{@article.id}/?key=#{@article.key}"
+    end  
 
     redirect_to category_article_url(params[:category_id], @article)
   end  
@@ -18,6 +19,22 @@ class ArticlesController < ApplicationController
     @category = Category.find(params[:category_id])
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @category = Category.find(params[:category_id])
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:category_id])
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to [@category, @article]
+    else
+      render 'edit'
+    end
+  end    
 
   private
     def article_params
