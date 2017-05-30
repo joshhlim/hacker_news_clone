46af1cb8334338cb164c8e54c85c8d8abdcaedb9
Paul Vander <paulsvander@gmail.com>
2017-05-29 20:38:23 -0700

    Create article now working with article show page linking back to category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,27 @@
 class ArticlesController < ApplicationController
 
+  def new
+    @category = Category.find(params[:category_id])
+    respond_with(@article)
+  end
+
   def create
-    @article = Article.new(article_params)
+    @category = Category.find(params[:category_id])
+    @article = @category.articles.create(article_params)
     @article.make_key
-    puts @article
     @article.save
-    redirect_to @arti
+
+    redirect_to category_article_url(params[:category_id], @article)
   end  
 
   def show
     @category = Category.find(params[:category_id])
-    @article = @category.articles.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   private
     def article_params
-      params.require(:article).permit(:title, :body, :price, :email)
+      params.require(:article).permit(:title, :body, :price, :email, :category_id)
     end
 
 end
